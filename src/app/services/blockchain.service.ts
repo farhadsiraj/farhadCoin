8c32ccf2a1dd56a34ee0e4e7626743545eef7aaf
Farhad Siraj <farhadsiraj@gmail.com>
2021-01-23 14:38:51 -0500

    feat: created block view component

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Blockchain } from 'blockchain/src/blockchain';
+import EC from 'elliptic';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BlockchainService {
+  public blockchainInstance = new Blockchain();
+  public walletKeys = [];
 
-  constructor() { }
+  constructor() {
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+
+    this.generateWalletKeys();
+  }
+
+  getBlocks() {
+    return this.blockchainInstance.chain;
+  }
+
+  private generateWalletKeys() {
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+  }
 }
